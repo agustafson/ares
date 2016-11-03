b8389439f448e72c03ae6406faf3b36cba51bd41
agustafson <andrew.gustafson@itv.com>
2016-11-03 22:35:05 +0000

    Define Strategy and AsynchronousChannelGroup

--- src/test/scala/ares/RedisTest.scala
+++ src/test/scala/ares/RedisTest.scala
@@ -1,8 +1,10 @@
 package ares
 
 import java.net.InetSocketAddress
+import java.nio.channels.AsynchronousChannelGroup
 
 import ares.interpreter.Fs2TaskInterpreter
+import fs2.Strategy
 import org.scalacheck.{Gen, Prop, Properties}
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
@@ -10,7 +12,11 @@ import org.specs2.specification.Scope
 
 class RedisTest extends Specification with ScalaCheck {
   trait RedisClientScope extends Scope {
-    val jInt = new Fs2TaskInterpreter(new InetSocketAddress("127.0.0.1", 6379))(SocketSpec.tcpACG, TestUtil.S)
+    private val threadName: String = "redis.threadfactory"
+    val strategy = Strategy.fromFixedDaemonPool(8, threadName)
+    val acg = AsynchronousChannelGroup.withFixedThreadPool(8, Strategy.daemonThreadFactory(threadName))
+
+    val jInt = new Fs2TaskInterpreter(new InetSocketAddress("127.0.0.1", 6379))(acg, strategy)
   }
 
   val p1: Properties = new Properties("send and receive content") with RedisClientScope {

89b3431475acf73665f442ea81ff575b7519c506
agustafson <andrew.gustafson@itv.com>
2016-11-04 17:10:42 +0000

    Remove commented out code

--- src/main/scala/ares/RedisCommands.scala
+++ src/main/scala/ares/RedisCommands.scala
@@ -14,19 +14,3 @@ import scala.language.higherKinds
   def get(key: String): CommandOp[Option[String]]
   def set(key: String, value: String): CommandOp[Either[ErrorReply, Unit]]
 }
-/*
-@free trait KVStore {                     // you can use any names you like
-type KVStoreF[A] = Free[GrammarADT, A]  // as long as you define a type alias for Free
-sealed trait GrammarADT[A]              // and a sealed trait.
-
-  // abstract methods are automatically lifted into part of the grammar ADT
-  def put[T](key: String, value: T): KVStoreF[Unit]
-  def get[T](key: String): KVStoreF[Option[T]]
-
-  def update[T](key: String, f: T => T): KVStoreF[Unit] =
-    for {
-      vMaybe <- get[T](key)
-      _      <- vMaybe.map(v => put[T](key, f(v))).getOrElse(Free.pure(()))
-    } yield ()
-}
-*/

7848f9f98873607b8418a92dfb685f770b309b68
agustafson <andrew.gustafson@itv.com>
2016-11-07 14:46:58 +0000

    Update RedisTest - add set/get option

--- build.sbt
+++ build.sbt
@@ -36,6 +36,7 @@ libraryDependencies ++= {
     "org.typelevel" %% "cats" % "0.7.2",
     "co.fs2" %% "fs2-core" % fs2,
     "co.fs2" %% "fs2-io" % fs2,
+    "co.fs2" %% "fs2-cats" % "0.1.0",
     "org.scalatest" %% "scalatest" % "3.0.0" % "test",
     "org.scalacheck" %% "scalacheck" % "1.13.4" % "test",
     "org.specs2"     %% "specs2-core"       % "3.8.4"  % "test",
--- src/test/scala/ares/RedisTest.scala
+++ src/test/scala/ares/RedisTest.scala
@@ -3,24 +3,47 @@ package ares
 import java.net.InetSocketAddress
 import java.nio.channels.AsynchronousChannelGroup
 
+import ares.RedisCommands._
 import ares.interpreter.Fs2TaskInterpreter
-import fs2.Strategy
+import cats.{Monad, RecursiveTailRecM}
+import cats.free._
+import fs2.{Strategy, Task}
+import fs2.interop.cats.monadToCats
 import org.scalacheck.{Gen, Prop, Properties}
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 import org.specs2.specification.Scope
 
 class RedisTest extends Specification with ScalaCheck {
+  implicit lazy val taskRecursiveTailRecM = new RecursiveTailRecM[Task] {}
+
   trait RedisClientScope extends Scope {
     private val threadName: String = "redis.threadfactory"
     val strategy = Strategy.fromFixedDaemonPool(8, threadName)
     val acg = AsynchronousChannelGroup.withFixedThreadPool(8, Strategy.daemonThreadFactory(threadName))
 
-    val jInt = new Fs2TaskInterpreter(new InetSocketAddress("127.0.0.1", 6379))(acg, strategy)
+    lazy val jInt = new Fs2TaskInterpreter(new InetSocketAddress("127.0.0.1", 6379))(acg, strategy)
+
+    def runCommand[T](op: ops.CommandOp[T]): T = {
+      jInt.run(op).unsafeRun()
+    }
   }
 
   val p1: Properties = new Properties("send and receive content") with RedisClientScope {
-    property("get unknown key returns None") = Prop.forAllNoShrink(Gen.const("helloworld"))((key: String) => jInt.get(key).unsafeRun() === None)
+    property("get unknown key returns None") = Prop.forAllNoShrink(Gen.const("helloworld")) { (key: String) =>
+      runCommand(ops.get(key)) === None
+    }
+
+    property("get unknown key returns None") = Prop.forAllNoShrink(Gen.const("foo"), Gen.const("bar")) { (key: String, value: String) =>
+      val command = for {
+        setResult <- ops.set(key, value)
+        getResult <- ops.get(key)
+      } yield (setResult, getResult)
+      val (setResult, getResult) = runCommand(command)
+      setResult === Right(())
+      getResult === Some(value)
+    }
+
   }
 
   s2"Can send and receive content$p1"

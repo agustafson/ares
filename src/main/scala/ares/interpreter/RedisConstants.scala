aa60181c0344b7b8c740a2dc7f9f3ba73eb8468d
agustafson <andrew.gustafson@itv.com>
2016-11-09 17:17:11 +0000

    Reformat RedisConstants

--- src/main/scala/ares/interpreter/RedisConstants.scala
+++ src/main/scala/ares/interpreter/RedisConstants.scala
@@ -1,12 +1,12 @@
 package ares.interpreter
 
 object RedisConstants {
-  val DOLLAR_BYTE: Byte = '$'
+  val DOLLAR_BYTE: Byte   = '$'
   val ASTERISK_BYTE: Byte = '*'
-  val PLUS_BYTE: Byte = '+'
-  val MINUS_BYTE: Byte = '-'
-  val COLON_BYTE: Byte = ':'
-  val CR: Byte = '\r'
-  val LF: Byte = '\n'
-  val CRLF: Vector[Byte] = Vector[Byte](CR, LF)
+  val PLUS_BYTE: Byte     = '+'
+  val MINUS_BYTE: Byte    = '-'
+  val COLON_BYTE: Byte    = ':'
+  val CR: Byte            = '\r'
+  val LF: Byte            = '\n'
+  val CRLF: Vector[Byte]  = Vector[Byte](CR, LF)
 }

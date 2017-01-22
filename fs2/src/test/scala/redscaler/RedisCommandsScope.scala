fbc11fcee95eedd534bb1496831da53b4346e75a
agustafson <andrew.gustafson@itv.com>
2017-01-22 18:27:23 +0000

    Remove printlns

--- fs2/src/test/scala/redscaler/RedisCommandsScope.scala
+++ fs2/src/test/scala/redscaler/RedisCommandsScope.scala
@@ -3,18 +3,11 @@ package redscaler
 import fs2.Task
 import fs2.interop.cats.monadToCats
 import org.specs2.execute.{AsResult, Result}
-import org.specs2.specification.{Context, Fixture}
 import redscaler.RedisCommands.ops
-import redscaler.RedisDatabaseScope.newRedisClient
-import redscaler.interpreter.Fs2CommandInterpreter
 
 trait RedisCommandsScope {
   implicit class CommandInterpreterW(val commandInterpreter: RedisCommands.Interp[Task]) {
-    def runCommand[T](op: ops.CommandOp[T]): T = {
-      commandInterpreter.run(op).unsafeRun() /*.fold[T]({ exception =>
-        println(s"Error occurred running $op against ${commandInterpreter}")
-      }, identity)*/
-    }
+    def runCommand[T](op: ops.CommandOp[T]): T = commandInterpreter.run(op).unsafeRun()
   }
 
   def withRepository[R: AsResult](f: RedisCommands.Interp[Task] => R): Result = {
@@ -22,12 +15,7 @@ trait RedisCommandsScope {
       try {
         db.selectDatabase()
         val result = f(db.commandInterpreter)
-        println(s"Result from running operation against db ${db.dbIndex}: $result")
         AsResult(result)
-      } catch {
-        case ex: Throwable =>
-          println(s"Error occurred running operation against db ${db.dbIndex}: $ex")
-          throw ex
       } finally {
         db.flushDb()
       }

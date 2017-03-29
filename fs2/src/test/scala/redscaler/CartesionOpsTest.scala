cc5575b9734ebc4f4a5c0cd46362bfa5d604377a
Andrew Gustafson <andrew.gustafson@digital.hmrc.gov.uk>
2017-03-29 21:28:53 +0100

    Add CartesionOpsTest

---
+++ fs2/src/test/scala/redscaler/CartesionOpsTest.scala
@@ -0,0 +1,18 @@
+package redscaler
+
+import cats.syntax.cartesian._
+import org.specs2.mutable.Specification
+import org.specs2.specification.Scope
+import redscaler.ConnectionOps.ops
+
+class CartesionOpsTest extends Specification with RedisClientScope {
+  "Cartesion ops" should {
+    "combine together" in new RedisCommandsScope with Scope {
+      withRepository { interpreter =>
+        val (key, value) = ("foo", "bar".getBytes.toVector)
+        val op = ops.set(key, value) *> ops.get(key)
+        interpreter.runCommand(op) ==== Right(Some(value))
+      }
+    }
+  }
+}
